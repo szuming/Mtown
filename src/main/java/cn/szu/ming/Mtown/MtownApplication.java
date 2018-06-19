aec45c0890b97908e52b32b5b971d2466af76b96
ming <2015150308@email.szu.edu.cn>
2018-06-19 19:50:24 +0800

    第一次提交

---
+++ src/main/java/cn/szu/ming/Mtown/MtownApplication.java
@@ -0,0 +1,12 @@
+package cn.szu.ming.Mtown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtownApplication.class, args);
+	}
+}
